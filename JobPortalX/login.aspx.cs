dbad02d38adda26bf4b49e1dca745bda1f0ee474
Shreevatsa Nayak <jansun2k@gmail.com>
2023-10-03 00:44:50 +0100

    Update login.aspx.cs
    Implemented SQL functionality

--- JobPortalX/login.aspx.cs
+++ JobPortalX/login.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +12,46 @@ namespace JobPortalX
 {
     public partial class login : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
+           
+        }
 
+      
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+           // Response.Write("<script>alert('Please fill out all fields.');</script>");
+            
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("select * from users_table where user_name='" + TextBox4.Text.Trim() + "' AND password='" + TextBox3.Text.Trim() + "'", con);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr .HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Response.Write("<script>alert('" + dr.GetValue(2).ToString() + "');</script>");
+                    }
+                    
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid User');</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
